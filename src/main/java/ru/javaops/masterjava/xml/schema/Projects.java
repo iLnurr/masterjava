6557bc6699647b987fb79bae1fc4927ed5eab2ce
ilnur <serbaevilnur@gmail.com>
2016-09-29 16:35:30 +0500

    create Projects class

--- src/main/java/ru/javaops/masterjava/xml/schema/Projects.java
+++ src/main/java/ru/javaops/masterjava/xml/schema/Projects.java
@@ -1,7 +1,13 @@
 package ru.javaops.masterjava.xml.schema;
 
+import javax.xml.bind.annotation.*;
+
 /**
  * Created by ilnur on 29.09.16.
  */
+@XmlRootElement
 public class Projects {
+    @XmlElement
+    private String name;
+    private String description;
 }
